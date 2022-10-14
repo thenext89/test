e396c9b50f19ad386a628f883929519dca78db9b
thenext89 <gc123580@gmail.com>
2022-10-14 16:09:22 +0700

    heh

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Thanh Hải à!',
+    introDesc: `<3 Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'THANH HẢI à anh chỉ muốn nói với em rằng anh đã yêu em từ cái nhìn đầu tiên rồi 😙',
+    desc: ' em có thích anh không (a Long phòng dev)',
+    btnYes: 'Em thích anh lắm -<33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại thích anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'yêu em.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.messenger.com/t/100033489978750' 
+}
